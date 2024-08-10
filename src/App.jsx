bd32d15cfa0a49390d4859b36fc91e20441fa905
Alishan khattak <99354261+ali-khattak@users.noreply.github.com>
2024-08-10 23:02:41 +0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from "react";
-import Home from "./pages/Home";
-import "./App.css";
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import CoursePage from './components/CoursePage';
+import Navbar from './components/Header';
+import SubCourseDetails from './components/SubCourseDetails';
+import MentorsPage from './components/MentorsPage';
+import BlogAndArticlesPage from "./components/BlogAndArticlesPage";
+import ArticleDetailsPage from './components/ArticleDetailsPage';
+import AboutPage from './components/AboutPage';
+import FAQPage from './components/FAQPage';
 
-const App = () => {
+
+function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<CoursePage />} />
+          <Route path="/subcourse-details" element={<SubCourseDetails />} />
+          <Route path="/mentors" element={<MentorsPage />} />
+          <Route path="/blog-articles" element={<BlogAndArticlesPage/>} />
+          <Route path="/articles/:id" element={<ArticleDetailsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+
+      </Routes>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
